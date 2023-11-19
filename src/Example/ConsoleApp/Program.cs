056024a4cecbd7d8356c48e6de424621d373092a
Denifia <mr.l.wale@gmail.com>
2023-11-19 20:17:12 +0800

    feat: added initial config

---
+++ src/Example/ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// !! Below is what FAILS the style rules !!
+
+// Constants should be PascalCase
+private const string myConst = "";
+
+// Static should start with s_
+private static string _myStatic = "";
+
+// Use var if type is obvious
+string s = "";
+
+// Don't have more than one blank line in a row
+
+
+// Use brackets for if statements
+if (true)
+    Console.WriteLine();
